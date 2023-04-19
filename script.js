0a84e001ce979f3059c184bdd656f4192760e0ac
Thapelo A Motsumi <Thapelo.umuziphotoclub@gmail.com>
2023-04-19 23:27:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/*This is the firstname of the user*/
+const user = 'John';
+const surname = 'Smith';
+
+console.log( "user:",user, surname);
+/*This is the lastname of the user const surname = 'Smith'*/
+
